ec769b6ff012447fe49d8e4ec88bfbb76c006a46
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-05-29 12:42:43 +0500

    Backend Domain Changed

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -1,25 +1,21 @@
-async function userLogout(req, res) {
-    try {
-        res.clearCookie('token', {
-            httpOnly: true,
-            secure: true, // Match the options used when setting the cookie
-            sameSite: 'None', // Match the options used when setting the cookie
-            path: '/', // Match the options used when setting the cookie
-        });
+async function userLogout(req,res){
+    try{
+        res.clearCookie("token")
 
         res.json({
-            message: 'Logged out successfully',
-            error: false,
-            success: true,
-            data: []
-        });
-    } catch (err) {
+            message : "Logged out successfully",
+            error : false,
+            success : true,
+            data : []
+        })
+    }catch(err){
         res.json({
-            message: err.message || err,
-            error: true,
-            success: false,
-        });
+            message : err.message || err  ,
+            error : true,
+            success : false,
+        })
     }
 }
 
-module.exports = userLogout;
+
+module.exports = userLogout
