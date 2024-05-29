35e28aa3a3150b06f31ef442bc9d3549c74f6034
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-05-29 12:20:19 +0500

    Changes made

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 
