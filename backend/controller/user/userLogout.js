6656f96db0939b4d6feda20d1f9f5555ecec1096
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-05-29 12:29:18 +0500

    Changes made in logout controller

--- backend/controller/user/userLogout.js
+++ backend/controller/user/userLogout.js
@@ -1,21 +1,25 @@
-async function userLogout(req,res){
-    try{
-        res.clearCookie("token")
+async function userLogout(req, res) {
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: true, // Match the options used when setting the cookie
+            sameSite: 'None', // Match the options used when setting the cookie
+            path: '/', // Match the options used when setting the cookie
+        });
 
         res.json({
-            message : "Logged out successfully",
-            error : false,
-            success : true,
-            data : []
-        })
-    }catch(err){
+            message: 'Logged out successfully',
+            error: false,
+            success: true,
+            data: []
+        });
+    } catch (err) {
         res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
+            message: err.message || err,
+            error: true,
+            success: false,
+        });
     }
 }
 
-
-module.exports = userLogout
+module.exports = userLogout;
